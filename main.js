cd73b7a0a0bffa32095b605a8698a6d8d68703a0
yoon-seo-yeon <s2013@e-mirim.hs.kr>
2022-06-13 10:19:58 +0900

    game_main 수정

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// let opeingAudio = new Audio("audio/opening.mp3");
+// opeingAudio.play();
+// alert("들어옴");
+function btn_click() {
+    // alert("zzz");
+    let node = document.querySelector('#audio');
+    node.play();
+    // alert("사운드 재생");
+}
