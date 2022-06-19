a2e0f7d90038757dceb5b9b6c80db99756b54663
조혜라 <s2037@e-mirim.hs.kr>
2022-06-19 14:11:10 +0900

    feat(rank) : 랭킹 출력하기 디자인 제외 (#1)

--- rankingShow.php
+++ rankingShow.php
@@ -8,14 +8,14 @@ $db = "bdk_db";
 $conn = mysqli_connect($url,$id,$password,$db);
 
 // 데이터베이스로부터 값 가져오기
-$sql="select * from game";
+$sql="select * from game order by score desc;";
 $result=mysqli_query($conn,$sql);
 $num=mysqli_num_rows($result);
 
 for($i=0;$i<$num;$i++) {
 
     $re=mysqli_fetch_array($result);
-    echo "이름 : ".$re[0]."   점수 : ".$re[1]."<br/>";
+    echo $re[0]."&nbsp;&nbsp;".$re[1]."<br/>";
 
 }
 
